06f4418114f1b3274d1c9998997d2e5f94c71bff
Nikita <ashtonashtonkachur@gmail.com>
2023-02-01 11:38:16 -0500

    resolve summ of two numbers O(n**2)

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+# arr = [2, 4, 3, 11, 0, 1]
+#
+#
+# def two_num_sum(array, target):
+#     for i in range(len(array)):
+#         for j in range(1, len(array)):
+#             if array[i] + array[j] == target:
+#                 return 1
+#     else:
+#         return -1
+#
+# print(two_num_sum(arr, 12))
+# Speed n**2
