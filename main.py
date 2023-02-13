4503f629b9d880bba34f7d5bf58307317e14c2b8
Nikita <ashtonashtonkachur@gmail.com>
2023-02-12 20:33:27 -0500

    remove_element problem

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
-arr = [1, 2, 3, 10]
-test_1 = [7,1,5,3,6,4]
-test_2 = [3,2,6,5,0,3]
-test_3 = [2,1,2,1,0,1,2]
+arr = [2, 1, 2,  0, 1, 2]
+
 
 def two_sum(nums, target):
     visited_nums = {}
@@ -37,51 +35,14 @@ is_palindrome(101)
 # Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
 
-# def max_profit(prices):
-#     if len(prices) == 1:
-#         return
-#     left_end = 0
-#     right_end = len(prices) - 1
-#     min_price = min(prices)
-#     max_price = max(prices)
-#     min_price_index = prices.index(min_price)
-#     max_price_index = prices.index(max_price)
-#     if min_price_index == right_end:
-#         prices.remove(min_price)
-#         case_1 = max_profit(prices)
-#         return case_1
-#     elif max_price_index == left_end:
-#         prices.remove(max_price)
-#         case_2 = max_profit(prices)
-#         return case_2
-#     elif max_price_index > min_price_index:
-#         res = max_price - min_price
-#         return res
-#
-#
-# solution = Solution()
-# print(solution.max_profit([2, 1, 11,]))
 
-def max_profit(array):
-    min_value = min(array)
-    max_value = max(array)
-    while len(array) > 1:
-        if min_value == 0:
-            array.remove(min_value)
-            min_value = min(array)
-        if array.index(min_value) == len(array) - 1:
-            if len(array) > 1:
-                array.remove(min_value)
-                min_value = min(array)
-        if array.index(max_value) < array.index(min_value):
-            if len(array) > 1:
-                array.remove(max_value)
-                max_value = max(array)
-        else:
-            return max_value - min_value
-    else:
-        return 0
+# 27. Remove Element
 
 
-print(max_profit(test_2))
+def remove_element(nums, val):
+    for i in range(len(nums)):
+        while val in nums:
+            nums.remove(val)
+    return nums
 
+# print(remove_element())
